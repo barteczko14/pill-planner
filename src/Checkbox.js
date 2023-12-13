cd8e565466ec19a4a7e6a9a4cad61c3ddf744a13
Bartek <bartek.kryczka@wp.pl>
2023-12-14 00:39:36 +0100

    refactoring code

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,42 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/database'
-import firebaseConfig from './firebaseConfig'
-
-firebase.initializeApp(firebaseConfig)
+import React, { useState, useEffect, useRef } from 'react'
 
 const Checkbox = ({ id }) => {
 	const [checked, setChecked] = useState(false)
 	const [clickTime, setClickTime] = useState(null)
-	const checkboxRef = firebase.database().ref(`checkboxes/${id}`)
-	const clickTimeRef = firebase.database().ref(`clickTimes/${id}`)
+	const CHECKBOXES_API = `https://pill-planner-default-rtdb.firebaseio.com/checkboxes/${id}.json`
+	const CLICK_TIMES_API = `https://pill-planner-default-rtdb.firebaseio.com/clickTimes/${id}.json`
+	const checkboxRef = useRef(CHECKBOXES_API)
+	const clickTimeRef = useRef(CLICK_TIMES_API)
 
 	useEffect(() => {
-		checkboxRef.on('value', snapshot => {
-			const isChecked = snapshot.val()
-			setChecked(isChecked)
-		})
+		const fetchData = async () => {
+			try {
+				const checkboxResponse = await fetch(checkboxRef.current)
+				const clickTimeResponse = await fetch(clickTimeRef.current)
 
-		clickTimeRef.on('value', snapshot => {
-			const time = snapshot.val()
-			setClickTime(time)
-		})
+				if (!checkboxResponse.ok || !clickTimeResponse.ok) {
+					throw new Error('Błąd pobierania danych')
+				}
 
-		return () => {
-			checkboxRef.off()
-			clickTimeRef.off()
+				const isChecked = await checkboxResponse.json()
+				const time = await clickTimeResponse.json()
+
+				setChecked(isChecked)
+				setClickTime(time)
+			} catch (error) {
+				console.error('Błąd pobierania danych:', error.message)
+			}
 		}
-	}, [id])
+
+		fetchData()
+
+		return () => {}
+	}, [id, checkboxRef, clickTimeRef])
 
 	useEffect(() => {
-		const check24Hours = () => {
+		const check24Hours = async () => {
 			if (clickTime) {
-				const now = new Date()
-				const clickDate = new Date(clickTime)
-				clickDate.setDate(clickDate.getDate() + 3)
-				if (now >= clickDate) {
-					setChecked(false)
-					checkboxRef.set(false)
+				try {
+					const now = new Date()
+					const clickDate = new Date(clickTime)
+					clickDate.setDate(clickDate.getDate() + 3)
+
+					if (now >= clickDate) {
+						setChecked(false)
+						await fetch(checkboxRef.current, {
+							method: 'PUT',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify(false),
+						})
+					}
+				} catch (error) {
+					console.error('Błąd podczas aktualizacji danych:', error.message)
 				}
 			}
 		}
@@ -48,11 +62,26 @@ const Checkbox = ({ id }) => {
 		}
 	}, [checkboxRef, clickTime])
 
-	const handleCheckboxChange = event => {
+	const handleCheckboxChange = async event => {
 		const isChecked = event.target.checked
-		setChecked(isChecked)
-		checkboxRef.set(isChecked)
-		clickTimeRef.set(firebase.database.ServerValue.TIMESTAMP)
+
+		try {
+			await fetch(checkboxRef.current, {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(isChecked),
+			})
+
+			await fetch(clickTimeRef.current, {
+				method: 'PUT',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(Date.now()),
+			})
+
+			setChecked(isChecked)
+		} catch (error) {
+			console.error('Błąd podczas aktualizacji danych:', error.message)
+		}
 	}
 
 	return (
